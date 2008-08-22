46e0a38f5275f3156599f95fe3e10f349003c4b9
Josh Ulrich <josh.m.ulrich@gmail.com>
2008-08-22 00:00:00 +0000

    version 0.1-1

--- R/pack.R
+++ R/pack.R
@@ -7,51 +7,58 @@
 function(template, ...) {
 
   # http://perldoc.perl.org/functions/pack.html
-  
+
   template <- unlist(strsplit(template,"\\s"))
   values <- list(...)
 
   types <- gsub('[0-9]|\\*','',template)
-  bytes <- gsub('[a-Z]|/','',template)
+  bytes <- gsub('[[:alpha:]]|/','',template)
   bytes <- gsub('\\*','-1',bytes)
-  bytes <- as.numeric(bytes)
+  suppressWarnings(bytes <- as.numeric(bytes))
   result <- NULL
 
   # Loop over template / value pairs
   shift <- 0
   for( i in 1:length(template) ) {
+        
+    value <- values[i-shift][[1]]
     type <- types[i]
     byte <- bytes[i]
 
-    # A null byte
-    if( type == 'x' ) {
+    # If template requires more args than given
+    if( is.null(value) ) {
       val <- as.raw(0)
       nul <- raw(0)
-      shift <- shift + 1
-    } else {
-      value <- values[[i-shift]]
-    }
-
-    # A null padded string
+    } else
     if( type == 'a' ) {
+      value <- as.character(value)
       # In the case of 'a*'
-      if( byte == -1 ) {
-        val <- charToRaw( value )
-        nul <- raw(0)
-      } else {
-        if( nchar(value) > byte )
-          stop(paste('list value (',value,') too large for template value',sep=''))
-        val <- charToRaw( value )
-        nul <- rep( as.raw(0), byte-nchar(value) )
-      }
+      if( byte == -1 )
+        byte <- nchar(value)
+      if( nchar(value) > byte )
+        stop(paste('list value (',value,') too large for template value',sep=''))
+      val <- charToRaw( value )
+      nul <- rep( as.raw(0), byte-nchar(value) )
     } else
     # A space padded ASCII string
     if( type == 'A' ) {
+      value <- as.character(value)
       if( nchar(value) > byte )
         stop(paste('list value (',value,') too large for template value',sep=''))
       val <- charToRaw( value )
       nul <- rep( charToRaw(' '), byte-nchar(value) )
     } else
+    # Bit string, low-to-high order
+    if( type == 'b' ) {
+      val <- packBits( value )
+      nul <- raw(0)
+    } else
+    # Bit string, high-to-low order
+    if( type == 'B' ) {
+      val <- rev( packBits( value ) )
+      nul <- raw(0)
+    } else
+    # A null padded string
     # An unsigned char (octet) value.
     if( type == 'C' ) {
       val <- numToRaw( value, 1 )
@@ -67,14 +74,24 @@ function(template, ...) {
       val <- numToRaw( value, 4 )
       nul <- raw(0)
     } else
+    # A double-precision float in the native format.
+    # A single-precision float in the native format.    
     # Packed item count followed by packed items
-    if( regexpr('/',type) ) {
+    if( regexpr('/',type)>0 ) {
       seq <- unlist(strsplit(type,'/'))
       len <- nchar(value)
       num <- pack(seq[1], len)
       val <- pack(paste(seq[2],len,sep=''),value)
       val <- c(num,val)
       nul <- raw(0)
+    } else
+    # A null byte
+    if( type == 'x' ) {
+      val <- as.raw(0)
+      nul <- raw(0)
+      shift <- shift + 1
+    } else {
+      stop('\'',type,'\' is an unsupported template value')
     }
 
     # Combine result
--- R/unpack.R
+++ R/unpack.R
@@ -4,20 +4,22 @@
 #-------------------------------------------------------------------------#
 
 'unpack' <-
-function(template, values) {
+function(template, ...) {
 
   # http://perldoc.perl.org/functions/unpack.html
-  
+
   template <- unlist(strsplit(template,"\\s"))
+  values <- unlist(list(...))
 
   types <- gsub('[0-9]|\\*','',template)
-  bytes <- gsub('[a-Z]|/','',template)
+  bytes <- gsub('[[:alpha:]]|/','',template)
   bytes <- gsub('\\*','-1',bytes)
-  bytes <- as.numeric(bytes)
+  suppressWarnings(bytes <- as.numeric(bytes))
   result <- NULL
   
   # Loop over template / value pairs
   for( i in 1:length(template) ) {
+    
     type <- types[i]
     byte <- bytes[i]
 
@@ -27,27 +29,50 @@ function(template, values) {
       next
     }
 
+    # Check template and values length
+    if( length(values) == 0 ) {
+      val <- NULL
+      #stop('template values too long for binary data')
+    } else
+    # (decimal 240 would be hex F0.)
+    if( type == 'H' ) {
+      # In the case of 'H*'
+      if( byte == -1 )
+        byte <- length(values)
+      if( byte > length(values) )
+        stop('template too long for values')
+      val <- values[1:byte]
+      values <- values[-(1:byte)]
+    } else
     # A null padded string
     if( type == 'a' ) {
-      # In the case of 'a*'
-      if( byte == -1 ) {
-        val <- values
-      } else {
-        if( byte > length(values) )
-          stop('template too long for values')
-        val <- values[1:byte]
-        values <- values[-(1:byte)]
-      }
+      if( byte > length(values) )
+        stop('template too long for values')
+      val <- values[1:byte]
+      # strings can no longer have embedded nuls as of R-2.8.0
+      val <- rawToChar( val[as.logical(val)] )
+      values <- values[-(1:byte)]
     } else
     # A space padded ASCII string
     if( type == 'A' ) {
       if( byte > length(values) )
         stop('template too long for values')
       val <- values[1:byte]
-      val[val==0] <- charToRaw(' ')
-      val <- rawToChar( val )
+      # strings can no longer have embedded nuls as of R-2.8.0
+      val <- rawToChar( val[as.logical(val)] )
       values <- values[-(1:byte)]
     } else
+    # Bit string, low-to-high order
+    if( type == 'b' ) {
+      val <- rawToBits( values[1] )
+      values <- values[-1]
+    } else
+    # Bit string, high-to-low order
+    if( type == 'B' ) {
+      val <- rev( rawToBits( values[1] ) )
+      values <- values[-1]
+    } else
+    # Hex string - high nibble first
     # An unsigned char (octet) value.
     if( type == 'C' ) {
       val <- as.integer( values[1] )
@@ -76,14 +101,17 @@ function(template, values) {
       values <- values[-(1:4)]
     } else
     # Packed item count followed by packed items
-    if( regexpr('/',type) ) {
+    if( regexpr('/',type)>0 ) {
       seq <- unlist(strsplit(type,'/'))
-      num <- unpack(paste(seq[1],'a*'), values)
-      val <- unpack(paste(seq[2],num[[1]],' a*',sep=''),num[[2]])
+      num <- unpack(paste(seq[1],'H*'), values)
+      val <- unpack(paste(seq[2],num[[1]],' H*',sep=''),num[[2]])
       values <- val[[2]]
       val <- unlist(val[[1]])
+    } else
+    if( type != 'x' ) {
+      stop('\'',type,'\' is an unsupported template value')
     }
-    
+
     # Combine result
     result <- c(result,list(val))
   }
